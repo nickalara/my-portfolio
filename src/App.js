352b9487e379a9e4d0e866d45ac192a06d277efd
nickalara <nickalaradev@gmail.com>
2021-08-24 20:22:00 -0700

    commit before addressing 19 vulnerabilities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+//Remove boilerplate
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
   return (
+    //Remove boilerplate
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +22,8 @@ function App() {
         </a>
       </header>
     </div>
+    */
+    <div><h1>Placeholder</h1></div>
   );
 }
 
