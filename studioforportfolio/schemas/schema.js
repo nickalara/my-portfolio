7f40568368d07ec74d02187bed362e4aa5b6cb60
nickalara <nickalaradev@gmail.com>
2021-08-30 21:30:01 -0700

    finishing projects page and accompanying sanity schema

--- studioforportfolio/schemas/schema.js
+++ studioforportfolio/schemas/schema.js
@@ -6,9 +6,13 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
 import blockContent from './blockContent'
-import category from './category'
+//don't need category
+//import category from './category'
 import post from './post'
 import author from './author'
+
+//adding project to schema
+import project from './project'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -21,7 +25,10 @@ export default createSchema({
     // in the studio.
     post,
     author,
-    category,
+    //removing category
+    //category,
+    //adding project
+    project,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
